cb51a7d89556ecb9b0226cf824a6aac30c081a51
shpaksasha <shpaksasha0000@gmail.com>
2020-11-21 21:56:23 +0200

    app menu icon

--- src/pages/Header/header.js
+++ src/pages/Header/header.js
@@ -8,47 +8,47 @@ const AppMenu = () => {
     const classes = useStyles();
     return(
     <Toolbar className={classes.root} position='fixed'>
+        <div className={classes.links}>
+            <Typography className={classes.title}>
+                <NavLink exact={true} className={classes.link} activeClassName={classes.linkActive} to='/'>
+                    Buy tickets
+                </NavLink>
+            </Typography>
+            <Typography className={classes.title}>
+                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/manage'>
+                    Manage reservation
+                </NavLink>
+            </Typography>
+            <Typography className={classes.title}>
+                <NavLink className={classes.link}  activeClassName={classes.linkActive} to='/guided'>
+                    Guided tours
+                </NavLink>
+            </Typography>
+            <Typography className={classes.title}>
+                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/educational'>
+                    Educational tours
+                </NavLink>
+            </Typography>
+            <Typography className={classes.title}>
+                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/news'>
+                    News
+                </NavLink>
+            </Typography>
+            <Typography className={classes.title}>
+                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/help'>
+                    Help & FAQ
+                </NavLink>
+            </Typography>
+        </div>
         <Typography className={classes.icon}>
-            <Icon className={{height: '42px', width: '42px'}}>
+            <Icon className={{fontSize: 'large'}}>
                 <NavLink className={classes.eye} to='/'>
-                    <img src='/icon/ellipse.svg' alt="Logo"/>
-                    <span className={classes.see}><b>I see</b> you</span>
+                    <img src='/icon/shopping.svg' alt='shopping cart'/>
+                    <span className={classes.see}>Cart (0)</span>
                 </NavLink>
             </Icon>
         </Typography>
 
-        <div className={classes.links}>
-            <Typography className={classes.title}>
-                <NavLink exact={true} className={classes.link} activeClassName={classes.linkActive} to='/'>
-                    Home
-                </NavLink>
-            </Typography>
-            <Typography className={classes.title}>
-                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/services'>
-                    Services
-                </NavLink>
-            </Typography>
-            <Typography className={classes.title}>
-                <NavLink className={classes.link}  activeClassName={classes.linkActive} to='/technologies'>
-                    Technologies
-                </NavLink>
-            </Typography>
-            <Typography className={classes.title}>
-                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/portfolio'>
-                    Portfolio
-                </NavLink>
-            </Typography>
-            <Typography className={classes.title}>
-                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/about'>
-                    About us
-                </NavLink>
-            </Typography>
-            <Typography className={classes.title}>
-                <NavLink className={classes.link} activeClassName={classes.linkActive} to='/contacts'>
-                    Contacts
-                </NavLink>
-            </Typography>
-        </div>
     </Toolbar>
     )
 };
--- src/pages/Header/header.styles.js
+++ src/pages/Header/header.styles.js
@@ -10,4 +10,46 @@ export const useStyles = makeStyles(theme => ({
         justifyContent: 'space-between',
         boxSizing: 'border-box'
     },
+
+    links: {
+        display: 'flex',
+    },
+
+    title: {
+        paddingRight: '90px'
+    },
+
+    link: {
+        position: 'relative',
+        textDecoration: 'none',
+        color: '#828282',
+        fontSize: '15px',
+        lineHeight: '24px',
+        fontFamily: 'Roboto, sans-serif'
+    },
+
+    linkActive: {
+        paddingBottom: '5px',
+        borderBottom: '2px solid #37474f',
+        color: '#37474f',
+        fontSize: '16px',
+        fontFamily: 'Roboto, sans-serif'
+    },
+    eye: {
+        display: 'flex',
+        alignItems: 'center',
+        textDecoration: 'none',
+    },
+    icon: {
+        textDecoration: 'none',
+    },
+    see: {
+        width: '100%',
+        color: '#FFFFFF',
+        fontSize: '16px',
+        letterSpacing: '120%',
+        fontFamily: 'Montserrat',
+        paddingLeft: '8px',
+    },
+
 }))
