5538351c6cca0d25f492a5f1a055e92dd86fa906
shpaksasha <shpaksasha0000@gmail.com>
2020-11-24 22:52:18 +0200

    page number two

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import AppMenu from './pages/Header/header';
 import Home from './pages/Home/home';
 import {Route} from 'react-router-dom';
+import Manage from './pages/Manage/manage';
 
 
 
@@ -11,7 +12,7 @@ const App = () => {
         <Fragment>
             <AppMenu/>
             <Route exact path='/' component={Home}/>
-
+            <Route exact path='/manage' component={Manage}/>
         </Fragment>
     );
 }
---
+++ src/pages/Manage/manage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import tower from "../../background/greece.jpeg";
+
+const useStyles = makeStyles(theme =>({
+    root: {
+        backgroundImage: `url('${tower}')`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100vh',
+        margin: 0,
+        padding: 0,
+        position: 'relative',
+        // background: 'linearGradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
+        '&:before': {
+            content: "",
+            backgroundColor: 'rgba(0, 0, 0, 0.6)',
+            position: 'absolute',
+            bottom: 0,
+            top: 0,
+            left: 0,
+            right: 0,
+        }
+    }
+}))
+
+const Manage = () => {
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+
+        </div>
+    )
+};
+
+
+export default Manage;
