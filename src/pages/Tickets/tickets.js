3b7ef07f1491ff414afdb76a8d85902bb0da93b7
shpaksasha <shpaksasha0000@gmail.com>
2020-12-03 22:39:14 +0200

    changes on page tickets.js

--- src/pages/Tickets/tickets.js
+++ src/pages/Tickets/tickets.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import coliseum from '../../background/coliseum.jpg';
+import {Button, Card, CardActions, CardContent, Grid, Typography} from '@material-ui/core';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
+        // backgroundImage: `linear-gradient(0deg, #000000 1%, rgba(0, 0, 0, 0) 105%), url('${circle}')`,
         backgroundImage: `url('${coliseum}')`,
         backgroundPosition: 'bottom',
         backgroundSize: 'cover',
@@ -14,15 +17,55 @@ const useStyles = makeStyles(theme => ({
         margin: 0,
         padding: 0,
         position: 'relative',
+    },
+    block: {
+        position: 'absolute'
+    },
+
+    cards: {
+        position: 'relative'
     }
 }))
 
 
 const Tickets = () => {
     const classes = useStyles();
-    return(
+    return (
         <div className={classes.root}>
-
+            <Grid conteiner md={12} className={classes.block}>
+                <Typography>Online tickets</Typography>
+                <Typography>Select admission tickets:</Typography>
+                <Grid item md={6} >
+                    <Card className={classes.cards}>
+                        <CardContent>
+                            <Typography>
+                                Word of the Day
+                            </Typography>
+                            <Typography>
+                                adjective
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button>Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item md={6}>
+                    <Card className={classes.cards}>
+                        <CardContent>
+                            <Typography>
+                                Word of the Day
+                            </Typography>
+                            <Typography>
+                                adjective
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button>Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
         </div>
     )
 };
