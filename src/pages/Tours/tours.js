9c62051bf81826b5e322f9d2f9568680dcd8e904
shpaksasha <shpaksasha0000@gmail.com>
2020-12-08 22:41:16 +0200

    add keyboardDataPicker

--- src/pages/Tours/tours.js
+++ src/pages/Tours/tours.js
@@ -1,10 +1,13 @@
-import React,{Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import tours from '../../background/tours.jpg';
 import {Grid, Typography} from '@material-ui/core';
-import {KeyboardDatePicker} from '@material-ui/pickers';
+import {KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
 
-const useStyles = makeStyles(theme =>({
+
+const useStyles = makeStyles(theme => ({
     root: {
         backgroundImage: `url('${tours}')`,
         backgroundPosition: 'bottom',
@@ -36,37 +39,59 @@ const useStyles = makeStyles(theme =>({
         fontWeight: 'normal',
         color: '#ffffff',
     },
+    data: {
+
+        height: '15px',
+        color: '#ffffff',
+       background: 'rgba(0,0,0, 0.7)',
+    }
 }))
 
 const Tours = () => {
-    const [selectedDate, handleDateChange] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     const classes = useStyles();
-    return(
+
+    return (
         <div className={classes.root}>
-<Grid container md={12}>
-    <Grid item md={12} className={classes.firstBlock}>
-        <Typography className={classes.caption}>Vatican Museums and Sistine Chapel</Typography>
-        <Typography className={classes.tickets}>Select date and number of tickets</Typography>
-    </Grid>
-    <Grid item md={12}>
-        <Grid container md={12}>
-            <Grid item md={4}>
-                <Fragment>
-                <KeyboardDatePicker
-                    clearable
-                    value={selectedDate}
-                    placeholder="07/12/2020"
-                    onChange={date => handleDateChange(date)}
-                    minDate={new Date()}
-                    format="MM/dd/yyyy"
-                />
-                </Fragment>
+            <Grid container md={12}>
+                <Grid item md={12} className={classes.firstBlock}>
+                    <Typography className={classes.caption}>Vatican Museums and Sistine Chapel</Typography>
+                    <Typography className={classes.tickets}>Select date and number of tickets</Typography>
+                </Grid>
+                <Grid item md={12}>
+                    <Grid container md={12}>
+                        <Grid item md={4} className={classes.data}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <KeyboardDatePicker
+                                    // inputVariant='standard'
+                                    // disableToolbar='true'
+                                    inputVariant="outlined"
+                                    color='secondary'
+                                    id="date-picker-dialog"
+                                    label='1. Select date and time'
+                                    format='dd/MM/yyyy'
+                                    value={selectedDate}
+                                    onChange={handleDateChange}
+                                />
+                                <KeyboardTimePicker
+                                    pmam='true'
+                                    id='time'
+                                    label='.'
+                                    value={selectedDate}
+                                    onChange={handleDateChange}
+                                />
+                            </MuiPickersUtilsProvider>
+                        </Grid>
+                        {/*<Grid item md={4}></Grid>*/}
+                        {/*<Grid item md={4}></Grid>*/}
+                    </Grid>
+                </Grid>
             </Grid>
-            {/*<Grid item md={4}></Grid>*/}
-            {/*<Grid item md={4}></Grid>*/}
-        </Grid>
-    </Grid>
-</Grid>
         </div>
     )
 };
